4e5eb971e44a4f31dde5ceca3cf80a8df5bf2fe8
macbook kal <arnaud.pantenier@gmail.com>
2023-07-26 15:25:03 +0200

    Copier coller ancien dossier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Select from "./components/Select"
+
+const options = [
+  {label: "first", value: 1},
+  {label: "second", value: 2},
+  {label: "third", value: 3},
+  {label: "four", value: 4},
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
     </>
   )
 }
---
+++ src/components/Select.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+type SelectOption = {
+    label: string,
+    value: number
+}
+
+type SelectProp = {
+    options: SelectOption[],
+    value?: SelectOption,
+    onChange: (value: SelectOption | undefined) => void
+}
+
+
+const Select = ({value, onChange, options}: SelectProp) => {
+    
+    const [isOpen, setIsOpen] = useState(false);
+    
+    function clearOption() {
+        onChange(undefined)
+    }
+
+    function selectOption(option: SelectOption) {
+        onChange(option)
+    }
+
+    function isOptionSelected(option: SelectOption) {
+        return option === value
+    }
+
+    return (
+        <div className="container" tabIndex={0} onClick={() => (setIsOpen((prev)=>!prev))}>
+            <span className="value">{value?.label}</span>
+            <button className="btn-clear" onClick={(e) => {
+                e.stopPropagation()
+                clearOption()}}>&times;</button>
+            <div className="divider"></div>
+            <div className="caret"></div>
+            <ul className={isOpen ? "options show" : "options"}>
+                {options.map(option => (
+                    <li 
+                        className={isOptionSelected(option) ? "option selected" : "option" }
+                        key={option.label}
+                        onClick={(e)=>{
+                            e.stopPropagation()
+                            selectOption(option)
+                            setIsOpen(false)
+                        }}
+                    >{option.label}
+                    </li>
+                ) )}
+            </ul>
+        </div>
+    );
+};
+
+export default Select;
